351f1c44ad813ad6b85a56cfe8eec6a3a8b9ebac
Eduardo Ribeiro <166298206+EIAHRJAY@users.noreply.github.com>
2024-07-28 11:00:27 +0000

    update models.py

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, Planet, People
+from models import db, User, Planet, People,Favorites_People, Favorites_Planet
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -13,6 +13,7 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Planet, db.session))
     admin.add_view(ModelView(People, db.session))
-    #admin.add_view(ModelView(Favorite, db.session))
+    admin.add_view(ModelView(Favorites_People, db.session))
+    admin.add_view(ModelView(Favorites_Planet, db.session))
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -7,10 +7,15 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-   
+    
+    favorites_people = db.relationship("Favorites_People", back_populates="user")
+    favorites_planets = db.relationship("Favorites_Planet", back_populates="user")
+
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -29,6 +34,8 @@ class Planet(db.Model):
     climate = db.Column(db.String(80), nullable=False)
     terrain = db.Column(db.String(80), nullable=False)
 
+    favorites_planets = db.relationship("Favorites_Planet", back_populates="planet")
+    
     def __repr__(self):
         return '<Planet %r>' % self.name
 
@@ -40,6 +47,8 @@ class Planet(db.Model):
             "terrain": self.terrain
         }
     
+
+
 class People(db.Model):
     __tablename__ = 'people'
     id = db.Column(db.Integer, primary_key=True)
@@ -47,6 +56,8 @@ class People(db.Model):
     birth_year = db.Column(db.String(80), nullable=False)
     gender = db.Column(db.String(80), nullable=False)
 
+    favorites_people = db.relationship("Favorites_People", back_populates="people")
+    
     def __repr__(self):
         return '<People %r>' % self.name
 
@@ -57,3 +68,44 @@ class People(db.Model):
             "birth_year": self.birth_year,
             "gender": self.gender
         }
+
+
+
+class Favorites_People(db.Model):
+    __tablename__ = 'favorites_people'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship("User", back_populates="favorites_people")
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
+    
+    people = db.relationship("People", back_populates="favorites_people")
+
+     
+    def __repr__(self):
+         return '<Favorites_People %r>' % self.id
+
+    def serialize(self):
+         return {
+            "id": self.id,
+            "people_name": self.people.name
+         }
+
+
+
+class Favorites_Planet(db.Model):
+   __tablename__ = 'favorites_planet'
+   id = db.Column(db.Integer, primary_key=True)
+   user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+   user = db.relationship("User", back_populates="favorites_planets")
+   planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+   
+   planet = db.relationship("Planet", back_populates="favorites_planets")
+
+   def __repr__(self):
+         return '<Favorites_Planet %r>' % self.id
+
+   def serialize(self):
+        return {
+             "id": self.id,
+             "planet_name": self.planet.name if self.planet else None
+         }
